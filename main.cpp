9d7d6ec64e4c050b006c211749852e8b04d55626
IanD1013 <ian.tung1013@gmail.com>
2025-03-11 09:25:59 +1300

    Writing output to the console

--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
-int main() {
-    const double pi = 3.14;
+using namespace std;
 
-    int FileSize; // Pascal Case
-    int fileSize; // Camel Case
+int main() {
+    int x = 10;
+    int y = 20;
+    cout << "x = " << x << endl
+         << "y = " << y << endl;
+
+    double sales = 95000;
+    cout << "Sales: $" << sales << endl;
 
     return 0;
 }
