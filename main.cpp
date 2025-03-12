ec5681f68a9b3f142e9b614856ec8e665c634b75
IanD1013 <ian.tung1013@gmail.com>
2025-03-12 23:25:05 +1300

    Work with booleans

--- main.cpp
+++ main.cpp
@@ -4,5 +4,11 @@
 using namespace std;
 
 int main() {
-    cout << sizeof(int);
+    bool isNewUser = false;
+    cout << isNewUser << endl;
+    // true -> 1; false -> 0
+
+    cout << boolalpha << isNewUser << endl;
+    cout << noboolalpha << isNewUser << endl;
+    return 0;
 }
