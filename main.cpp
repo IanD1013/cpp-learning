fd0ef99b0cc52c40ef55f265775b12b945a4436f
IanD1013 <ian.tung1013@gmail.com>
2025-03-12 23:20:42 +1300

    Data types size and limits

--- main.cpp
+++ main.cpp
@@ -4,11 +4,5 @@
 using namespace std;
 
 int main() {
-    // cout << left;
-    // cout << setw(10) << "Spring" << setw(10) << "Nice" << endl
-    //      << setw(10) << "Summer" << setw(10) << "Hot";
-
-    cout << fixed << setprecision(10) << 12.34567;
-
-    return 0;
+    cout << sizeof(int);
 }
